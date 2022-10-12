145ac576c95203ad51a9daa51eceea355cf4dee2
Boluwatife-AJB <56480003+Boluwatife-AJB@users.noreply.github.com>
2022-10-12 13:34:16 +0100

    error on the mobile fit of the hero section

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -16,7 +16,31 @@ const AboutUs = () => (
       <div className="app__aboutus-content_about">
         <h1 className="headtext__cormorant">About Us</h1>
         <img src={images.spoon} alt="about_spoon" className="spoon__img" />
-        <p className="p__opensans"></p>
+        <p className="p__opensans">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui dolorum
+          ipsum deserunt mollitia sapiente nostrum cum maxime tenetur unde
+          porro!
+        </p>
+        <button type="button" className="custom__button">
+          Know more
+        </button>
+      </div>
+
+      <div className="app__aboutus-content_knife flex__center">
+        <img src={images.knife} alt="about-knife" />
+      </div>
+
+      <div className="app__aboutus-content_history">
+        <h1 className="headtext__cormorant">Our History</h1>
+        <img src={images.spoon} alt="about_spoon" className="spoon__img" />
+        <p className="p__opensans">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui dolorum
+          ipsum deserunt mollitia sapiente nostrum cum maxime tenetur unde
+          porro!
+        </p>
+        <button type="button" className="custom__button">
+          Know more
+        </button>
       </div>
     </div>
   </div>
