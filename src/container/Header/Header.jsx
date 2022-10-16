8f172d8b68dcdc65e64111c93a81508c983f46ad
Boluwatife-AJB <56480003+Boluwatife-AJB@users.noreply.github.com>
2022-10-16 22:31:51 +0100

    fixed the issue

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -27,11 +27,17 @@ const Header = () => (
   <div className="app__header app__wrapper section__padding" id="home">
     <div className="app__wrapper_info">
       <SubHeading title="Chase the new flavour" />
-      <h1 className="app__header-h1">The Key to Fine Dinning</h1>
+      {/* <h1 className="app__header-h1">The Key to Fine Dinning</h1>s */}
+
+      <p className="p__opensans" style={{ margin: '2rem 0' }}>
+        Sit tellus lobortis sed senectus vivamus molestie. Condimentum volutpat
+        morbi facilisis quam scelerisque sapien. Et, penatibus aliquam amet
+        tellus
+      </p>
     </div>
 
     <div className="app__wrapper_img">
-      <img src={images.welcomessss} alt="" />
+      <img src={images.welcome} alt="hero-img" />
     </div>
   </div>
 );
