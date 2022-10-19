1c4cd1f6dca2a067d55a4135265aa82975a81307
Boluwatife-AJB <56480003+Boluwatife-AJB@users.noreply.github.com>
2022-10-19 10:28:58 +0100

    project completed

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-
-import { FiFacebook, FITwitter, FiInstagram } from 'react-icons/fi';
+import { FiFacebook, FiTwitter, FiInstagram } from 'react-icons/fi';
+import { images } from '../../constants';
 import { FooterOverlay, Newsletter } from '../../components';
 import './Footer.css';
 
@@ -8,6 +8,47 @@ const Footer = () => (
   <div className="app__footer section__padding">
     <FooterOverlay />
     <Newsletter />
+
+    <div className="app__footer-links">
+      <div className="app__footer-links_contact">
+        <h1 className="app__footer-headtext">Contact Us</h1>
+        <p className="p__opensans">
+          57, Broad Street, Lagos Island, Lagos Nigeria
+        </p>
+        <p className="p__opensans">+234 212-344-1230</p>
+        <p className="p__opensans">+234 212-555-1230</p>
+      </div>
+      <div className="app__footer-links_logo">
+        <img src={images.gericht} alt="footer-logo" />
+        <p className="p__opensans">
+          &quot;The best way to find yourself is to lose yourself in the service
+          of others.&quot;
+        </p>
+        <img
+          src={images.spoon}
+          alt="spoon-img"
+          className="spoon__img"
+          style={{ marginTop: 15 }}
+        />
+        <div className="app__footer-links_icons">
+          <FiTwitter />
+          <FiInstagram />
+          <FiFacebook />
+        </div>
+      </div>
+
+      <div className="app__footer-links_work">
+        <h1 className="app__footer-headtext">Working Hours</h1>
+        <p className="p__opensans">Monday-Friday:</p>
+        <p className="p__opensans">08:00 am - 12:00 am</p>
+        <p className="p__opensans">Saturday-Sunday:</p>
+        <p className="p__opensans">07:00 am - 11:00 pm</p>
+      </div>
+    </div>
+
+    <div className="footer__copyright">
+      <p className="p__opensans">&copy; 2022 All rights reserved.</p>
+    </div>
   </div>
 );
 
